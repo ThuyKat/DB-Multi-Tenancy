21d4339da6e5f94a231e89df6eeba5b3fb6d5933
Zavis <thezavis@gmail.com>
2025-02-18 02:04:45 +1100

    DataSource config, createDataSource, addDataSource, MultiTenantDataSource class

--- src/main/java/multi_tenant/db/navigation/Config/DataSourceConfig.java
+++ src/main/java/multi_tenant/db/navigation/Config/DataSourceConfig.java
@@ -1,5 +1,44 @@
 package multi_tenant.db.navigation.Config;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import multi_tenant.db.navigation.Entity.Tenant;
+import multi_tenant.db.navigation.Service.TenantService;
+import multi_tenant.db.navigation.Utils.DataSourceUtil;
+import multi_tenant.db.navigation.Utils.MultiTenantDataSource;
+
+@Configuration
 public class DataSourceConfig {
 
+	@Autowired
+	TenantService tenantService;	
+	
+	@Autowired
+	private MultiTenantDataSource multiTenantDataSource;
+	
+	@Autowired
+	private DataSourceUtil dataSourceUtil;
+//	private final Map<Object, Object> dataSourceMap = new ConcurrentHashMap<>();	
+	
+	@Bean
+	public DataSource dataSource() {
+		List<Tenant> tenants = tenantService.getAllTenant();
+		for(Tenant tenant: tenants) {
+			multiTenantDataSource.addDataSource(tenant.getDbName(), dataSourceUtil.createDataSource(tenant.getDbName()));
+		}	
+		multiTenantDataSource.setDefaultTargetDataSource(dataSourceUtil.createDataSource("global_multi_tenant"));
+		multiTenantDataSource.afterPropertiesSet(); //reload database list
+		
+		
+		return multiTenantDataSource;
+	}
+	
 }
