fbeb9e295d2e3b9c5200e22e0e23f5b5022c17c7
Zavis <thezavis@gmail.com>
2025-02-24 02:33:53 +1100

    General delete unused class and comment class MultitenantProvider

--- src/main/java/multi_tenant/db/navigation/Utils/MultiTenantConnectionProvider.java
+++ src/main/java/multi_tenant/db/navigation/Utils/MultiTenantConnectionProvider.java
@@ -1,26 +1,37 @@
-package multi_tenant.db.navigation.Utils;
-
-import javax.sql.DataSource;
-
-import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MultiTenantConnectionProvider extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl{
-	@Autowired
-	private TenantRoutingDataSource tenantRoutingDataSource;
-	
-	@Override
-	protected DataSource selectAnyDataSource() {
-		return tenantRoutingDataSource.getDefaultDataSource();
-	}
-	
-
-	@Override
-	protected DataSource selectDataSource(Object tenantIdentifier) {
-		String currentTenant = TenantContext.getCurrentTenant();
-		return tenantRoutingDataSource.getDataSource(currentTenant);
-	}
-
-}
+//package multi_tenant.db.navigation.Utils;
+//
+//import java.io.Serializable;
+//import javax.sql.DataSource;
+//import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+////@Component
+//public class MultiTenantConnectionProvider extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl implements Serializable {
+//
+//    private static final long serialVersionUID = 1L;
+//
+//    @Autowired
+//    private TenantRoutingDataSource tenantRoutingDataSource;
+//
+//    @Override
+//    protected DataSource selectAnyDataSource() {       
+//        return tenantRoutingDataSource.getDefaultDataSource();
+//    }
+//
+//    @Override
+//    protected DataSource selectDataSource(Object tenantIdentifier) {
+//        if (tenantIdentifier == null) {           
+//            return tenantRoutingDataSource.getDefaultDataSource();
+//        }
+//
+//        String tenant = tenantIdentifier.toString();        
+//
+//        DataSource dataSource = tenantRoutingDataSource.getDataSource(tenant);
+//        if (dataSource == null) {           
+//            return tenantRoutingDataSource.getDefaultDataSource();
+//        }
+//
+//        return dataSource;
+//    }
+//}
