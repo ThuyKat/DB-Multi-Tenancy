f3b750bd0c1bf6153c77e191489719b3cbc43ef2
Zavis <thezavis@gmail.com>
2025-02-24 02:35:28 +1100

    Config DataSourceConfig remove Flyway PostConstruct, only use in application.properties

--- src/main/java/multi_tenant/db/navigation/Config/DataSourceConfig.java
+++ src/main/java/multi_tenant/db/navigation/Config/DataSourceConfig.java
@@ -1,30 +1,28 @@
 package multi_tenant.db.navigation.Config;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import javax.sql.DataSource;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManagerFactory;
 import multi_tenant.db.navigation.Utils.DataSourceUtil;
 
 @Configuration
 @EnableJpaRepositories(
-    basePackages = "multi_tenant.db.navigation.Repository.Global",
-    entityManagerFactoryRef = "globalEntityManagerFactory",
+    basePackages = "multi_tenant.db.navigation.Repository.Global", //only scan respo in this package
+    entityManagerFactoryRef = "globalEntityManagerFactory", 
     transactionManagerRef = "globalTransactionManager"
 )
 public class DataSourceConfig {
@@ -32,45 +30,41 @@ public class DataSourceConfig {
     @Autowired
     private DataSourceUtil dataSourceUtil;
 
+    //1. create global datasource 
     @Bean(name = "globalDataSource")
+    @Primary
     public DataSource globalDataSource() {
         return dataSourceUtil.createDataSource("global_multi_tenant");
     }
     
+    //2 EntityManagerFactoryBuilder
     @Bean
     public EntityManagerFactoryBuilder entityManagerFactoryBuilder() {
         return new EntityManagerFactoryBuilder(
-                new HibernateJpaVendorAdapter(),
-                new HashMap<>(), 
+                new HibernateJpaVendorAdapter(), //set Hibernate as a JPA Provider
+                new HashMap<>(), //show-sql, hibernate.ddl-auto
                 null
         );
     }
-
-    @Bean(name = "globalEntityManagerFactory")
-//    @DependsOn("flyway") // Flyway must run before Hibernate
+    
+    //3: create EntityManagerFactory (run after flyway)
+    @Bean(name = "globalEntityManagerFactory")   
     public LocalContainerEntityManagerFactoryBean globalEntityManagerFactory(
             EntityManagerFactoryBuilder builder,
             @Qualifier("globalDataSource") DataSource dataSource) {
+    	//return EntityManagerFactory
         return builder
                 .dataSource(dataSource)
-                .packages("multi_tenant.db.navigation.Entity.Global")
+                .packages("multi_tenant.db.navigation.Entity.Global") //only scan Entities inside this package
                 .persistenceUnit("globalPU")
                 .build();
     }
 
+    //4: create TransactionManager for Global Database - CRUD with db @Transactional-> rollback
     @Bean(name = "globalTransactionManager")
     public PlatformTransactionManager globalTransactionManager(
             @Qualifier("globalEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory);
     }
 
-    @PostConstruct
-    public void runFlywayMigration() {
-        Flyway flyway = Flyway.configure()
-                .dataSource(dataSourceUtil.createDataSource("global_multi_tenant"))
-                .locations("classpath:db/migration/global")
-                .baselineOnMigrate(true)
-                .load();
-        flyway.migrate();
-    }
 }
